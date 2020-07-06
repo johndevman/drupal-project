dfe78d911e26eafcc81cf32749099357a14e4422
John Svensson <john@kodamera.se>
2020-07-06 19:29:25 +0200

    created unit test

---
+++ web/modules/custom/example/tests/src/Unit/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\Tests\example\Unit;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Example test.
+ *
+ * @group test
+ */
+class ExampleTest extends UnitTestCase {
+
+  /**
+   * Tests true is true.
+   */
+  public function testTest() {
+    $this->assertTrue(TRUE);
+  }
+
+}
